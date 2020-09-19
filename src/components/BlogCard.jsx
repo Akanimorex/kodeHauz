e71575e36a297b8a2c769792b1daf103f9c71425
Akanimorex <akanimoime2016@gmail.com>
2020-09-19 14:13:59 +0100

    no styling,still raw

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import '../styles/blog.css';
+import { Redirect, Link } from 'react-router-dom';
+import SingleBlog from './SingleBlog';
 
 
 const BlogCard =(props)=>{
 
-    const viewPost = ()=>{
-        
-    }
+   
 
     const {articleInfo} = props;
-    return(
-             
+    return(           
             <div className="blog-card mt-4">
                 <h2>{articleInfo.title}</h2>
-                <p>{articleInfo.content.substring(0,300)} <span style={{color:'grey',cursor:'pointer'}} onClick={viewPost}> Read More...</span></p>
+                <p>{articleInfo.content.substring(0,300)} <Link to={`/blogpost/${articleInfo.id}`} style={{color:'grey',cursor:'pointer'}}> Read More...</Link></p>
                   <button className="btn btn-primary m-2">Edit</button>
                   <button className="btn btn-primary m-2">Delete</button>
             </div>
